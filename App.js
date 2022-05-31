d0cef2ae05439ae55ecc79307d4af4f6191f8463
KadirCanT <100346586+KadirCanT@users.noreply.github.com>
2022-05-31 13:25:22 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Quiz from './src/screens/Quiz'
+const App = () => {
+
+  return (
+    <Quiz />
+  );
+};
+
+
+export default App;
---
+++ src/data/QuizData.js
@@ -0,0 +1,27 @@
+export default QuizData = [
+    {
+        question: "Atom bombası nereye atıldı?",
+        options: ["Ankara","Çorum","Hiroşima ","Nagazaki"],
+        correct_option: ["Hiroşima","Nagazaki"]
+    },
+    {
+        question: "Hangisi karada hareket etmez?",
+        options: ["Araba","Tren","Gemi","Tır"],
+        correct_option: "Gemi"
+    },
+    {
+        question: "Aşağıdakilerden en büyük olan sayıyı seçiniz?",
+        options: ["4","45","99","1"],
+        correct_option: "99"
+    },
+    {
+        question: "Bir günde kaç saat vardır?",
+        options: ["12","18","1","24"],
+        correct_option: "24"
+    },
+    {
+        question: "Hangi hayvan diğerlerine göre en büyüktür?",
+        options: ["Fil","Kedi","Yunus","Tilki"],
+        correct_option: "Fil"
+    }
+]
